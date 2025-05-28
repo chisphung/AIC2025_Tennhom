f6a5d7a4d50badb895d336217b7bf67aa17d2a97
phungminhchi2k5 <phungminhchi2005@gmail.com>
2025-05-28 11:18:56 +0700

    first commit

--- train/model/train.py
+++ train/model/train.py
@@ -18,3 +18,16 @@ if __name__ == '__main__':
     yaml_path = "./fisheye8k.yaml"
     check_path(yaml_path)
     model = RTDETR('./SO_DETR/ultralytics/cfg/models/A-Test-r50-M.yaml')
+    model.train(data=str(yaml_path),
+                cache=False,
+                imgsz=1280,
+                epochs=250,
+                batch=2,
+                workers=4,
+                device='0',
+                # resume='', # last.pt path
+                project='sodetr',
+                name='sodetr_r50',
+                patience = 40,
+                # find_unused_parameters=True,
+                )
