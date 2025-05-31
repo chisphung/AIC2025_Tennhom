b086cb5897257c04c556df036d03c03d81bbd29c
phungminhchi2k5 <phungminhchi2005@gmail.com>
2025-05-31 22:06:45 +0700

    add get_fps, modify export_onnx

--- train/tools/export_onnx.py
+++ train/tools/export_onnx.py
@@ -1,18 +1,16 @@
 from ultralytics import RTDETR
 import sys
 
-def export_onnx(model_path, output_path):
-    model = RTDETR(model_path, task='detect')
-    model.export(format='onnx', output=output_path)
-    print(f"Model exported to {output_path}")
+def export_onnx(model_path):
+    model = RTDETR(model_path)
+    model.export(format='onnx')
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python export_onnx.py <model_path> <output_path>")
+    if len(sys.argv) != 2:
+        print("Usage: python export_onnx.py <model_path> ")
         sys.exit(1)
     
     model_path = sys.argv[1]
-    output_path = sys.argv[2]
     
-    export_onnx(model_path, output_path)
+    export_onnx(model_path)
     print("Export completed.")
